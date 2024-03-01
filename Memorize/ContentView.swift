b04efcb0908143af275e5af3f1c297961e016b00
Antonio <antonio@antonios-mbp-2.mynetworksettings.com>
2024-03-01 14:56:17 -0500

    Went over Z and H stacks, went over structs. Created CardView and implemented into Content View. Create isFaceupBool

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,52 @@
 
 import SwiftUI
 
+//struct ContentView: View { Default
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, CS193P!")
+//        }
+//        // Changes everything insode image and text
+//        .padding()
+//        .font(.largeTitle)
+//        
+//    }
+//}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView()
         }
+        // Changes everything insode image and text
+        .foregroundColor(.orange)
+        //        .fo nt(.largeTitle)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View{
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)                }
+            })
+        }
+    }
 
 #Preview {
     ContentView()
