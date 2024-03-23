1aefd7bace7ea03337e8dfc20bb91e9581e026b9
Antonio <antonio@antonios-mbp-2.mynetworksettings.com>
2024-03-22 23:09:04 -0400

    added cards array and Card Struct

--- MemorizeGame.swift
+++ MemorizeGame.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 
-struct MemoryGame {
+struct MemoryGame<CardContent>{
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+         
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+        
+    }
     
 }
