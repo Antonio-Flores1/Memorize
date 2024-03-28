aaea8610726557cb96d723c4f88626e55265a26d
Antonio <antonio@antonios-mbp-2.mynetworksettings.com>
2024-03-27 23:06:22 -0400

    fixed ascpect ratio of emojis on cards, fixed bug where emoji may not appear at times

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -51,10 +51,11 @@ struct EmojiMemoryGameView: View {
     
     //Cards Not a view builder HStack is a view builder
     var cards: some View {
-        LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
             ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(card: viewModel.cards[index])
+                CardView(viewModel.cards[index])
                     .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
             }
         } 
         .foregroundColor(.orange)
@@ -68,6 +69,11 @@ struct EmojiMemoryGameView: View {
 
 struct CardView: View {
     var card: MemoryGame <String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
     var body: some View{
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
@@ -75,9 +81,14 @@ struct CardView: View {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text(card.content).font(.largeTitle)
-                base.fill().opacity(card.isFaceUp ? 0 : 1)
+                Text(card.content)
+                    .font(.system(size: 85))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
             }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1)
         }
     }
 }
